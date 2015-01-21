077ebe999e0d21a579ff9be5c8f6dd1c11846366
Jay Abdallah <jay.h.abdallah@gmail.com>
2015-01-20 22:48:08 -0500

    average point function

---
+++ 1-20-2015.hs
@@ -0,0 +1,9 @@
+
+pointAverage x xTwo = (x + xTwo) / 2 
+
+--The midpoint formula is (x1 + x2) / 2  ,   (y1 + y2)  / 2.   Haskell is not going to print those values out in 
+--coordinate notation. There can't be a midpoint function. There can be an average point function, which is exactly what the midpoint formula is,
+--except that it applies that average twice. One for the y values, and one for the x values. The two average points are the new x and y coordinates. 
+
+--So what is the midpoint of a line segment/ It is the 
+
